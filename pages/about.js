6c10993202acc93cc6a0ec3bea1ac3b889d70d28
Forza5 <ganp433@gmail.com>
2023-01-16 19:58:09 +0900

    Nextjs pre-rendering 및 장점

--- pages/about.js
+++ pages/about.js
@@ -6,3 +6,13 @@ export default function Next(){
 // 반드시 export default를 해주어야 함!!
 // index 파일은 url = '/' / '/index'로 표현하지 않음!!
 // jsx를 사용하고 있다면 React.js를 import할 필요가 없음
+// Next.js는 앱에 있는 페이지들이 미리 렌더링 된다!!
+
+/**
+ * create-react-app
+ * => client-side rendering
+ * = 나의 브라우저가 유저가 보는 UI를 만드는 모든 것을 한다
+ * = 브라우저가 자바스크립트를 가지고와서 client-side의 자바스크립트가 모든 UI를 만든다.
+ * -- 오로지 <div></div> 하나만 유저가 보는 것이기 때문
+ * => 쉽게 말해, 브라우저가 말 그대로 모든 것을 함
+ */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,31 @@
+import {useState} from "react";
+
 export default function Home(){
-    return "nextjs";
+    const [counter, setCounter] = useState(0);
+    return (
+        <div>
+            <h1>Nextjs {counter}</h1>
+            <button onClick={() => setCounter(prev => prev + 1)}>+</button>
+        </div>
+        // Nextjs는 우리 앱의 초기 상태를 활용해서 미리 렌더링되어 옴
+        // => pre-rendering
+        // 페이지가 로딩될 때 많은 스크립트를 같이 요청하는데, react.js가 넘겨받아서 잘 작동함
+        // 페이지를 열자마자 보이는 것 => HTML
+        // -> 그러고나서 react.js가 클라이언트로 전송되었을 때, react.js앱이 된다.
+        // => react.js를 프론트엔드 안에서 실행하는 이런 것을 hydration이라고 부른다!!
+        // => Next.js는 react.js를 백엔드에서 동작시켜서 이 페이지를 미리 만드는데 이 것이 component를 렌더링한다.
+        // => 렌더링이 끝났을 때, HTML이 됨
+        // => Next.js는 그 HTML을 페이지의 소스코드에 넣어준다.
+        // => 유저는 자바스크립트와 react.js가 로딩되지 않았더라도 콘텐츠를 볼 수 있게 된다.
+
+        /**
+         * Next.js의 장점
+         * - 유저가 웹사이트에 가면, 초기 상태의 component로 된 미리 생성된 HTML 페이지를 보게 되고,
+         *   상호작용이 일어나면 react.js는 그걸 받아서 아주 잘 작동을 한다. 
+         * - 자바스크립트가 동작하지 않더라도 유저들은 앱의 어떠한 것들은 볼 수 있다.
+         * - 모든게 다 로딩이 되면 react.js가 연결되면서 우리가 원하는 것들을 얻게 된다.
+         */
+    )
 }
 
 /**
