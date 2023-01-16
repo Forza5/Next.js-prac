a217ccb6b84b2d8e3f047b18510476d2772bcf42
Forza5 <ganp433@gmail.com>
2023-01-16 21:24:52 +0900

    Nextjs css-modules 사용방법 및 특징

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,21 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
+
 export default function NavBar() {
     const router = useRouter();
     console.log(router);
     return (
+        // module.css => 자바스크립트 오브젝트에서의 프로퍼티 형식으로 적음
+        // 페이지가 빌드될 때, Next.js가 클래스이름을 무작위로 바꿔줌
+        // => 클래스이름 충돌을 겪지 않아도 됨
       <nav>
         <Link href="/">
-          <a style={{color: router.pathname === "/" ? "red" : "blue"}} className="hello">Home</a>
+            {/* 조건부 삽입문 => css modules(``백틱을 사용해도 되고, 배열[]을 사용해도 됨) */}
+          <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
         </Link>
         <Link href="/about">
-            <a style={{color: router.pathname === "/about" ? "red" : "blue"}}>About</a>
+            <a className={[styles.link, router.pathname === "/about" ? styles.active : ""].join(" ")}>About</a>
         </Link>
       </nav>
     );
