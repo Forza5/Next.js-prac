12080557610b2ac6b3afc11c3cd659ec5c24f5e7
Forza5 <ganp433@gmail.com>
2023-01-16 19:29:20 +0900

    nextjs Pages 특징

--- pages/about.js
+++ pages/about.js
@@ -2,3 +2,7 @@ export default function Next(){
     return "Nextjs is good";
 }
 
+// next.js는 pages의 있는 파일 명을 기준으로 가져다가 url의 이름으로 사용함!!
+// 반드시 export default를 해주어야 함!!
+// index 파일은 url = '/' / '/index'로 표현하지 않음!!
+// jsx를 사용하고 있다면 React.js를 import할 필요가 없음
